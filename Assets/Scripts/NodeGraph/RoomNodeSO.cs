6099c2661672e27846419a462f5bdb6622a1c706
PabloCS <titanbenevolo@icloud.com>
2023-01-26 16:38:06 -0300

    Room nodes with the editor.

--- Assets/Scripts/NodeGraph/RoomNodeSO.cs
+++ Assets/Scripts/NodeGraph/RoomNodeSO.cs
@@ -1,14 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class RoomNodeSO : ScriptableObject
 {
     [HideInInspector] public string id;
     [HideInInspector] public List<string> parentRoomNodeIDList = new List<string>();
     [HideInInspector] public List<string> childRoomNodeIDList = new List<string>();
-    [HideInInspector] public RoomNodeGraphSO nodeRoomGraph;
+    [HideInInspector] public RoomNodeGraphSO roomNodeGraph;
     [HideInInspector] public RoomNodeTypeListSO roomNodeTypeList;
 
     public RoomNodeTypeSO roomNodeType;
+
+    #region Editor Code
+
+#if UNITY_EDITOR
+
+    [HideInInspector] public Rect rect;
+
+    public void Initialise(Rect rect, RoomNodeGraphSO nodeGraph, RoomNodeTypeSO roomNodeType)
+    {
+        this.rect = rect;
+        this.id = Guid.NewGuid().ToString();
+        this.name = "RoomNode";
+        this.roomNodeGraph = nodeGraph;
+        this.roomNodeType = roomNodeType;
+
+        roomNodeTypeList = GameResources.Instance.roomNodeTypeList;
+    }
+
+   public void Draw(GUIStyle nodeStyle)
+    {
+        GUILayout.BeginArea(rect, nodeStyle);
+        EditorGUI.BeginChangeCheck();
+
+        int selected = roomNodeTypeList.list.FindIndex(x => x == roomNodeType);
+        int selection = EditorGUILayout.Popup("", selected, GetRoomNodeTypesToDisplay());
+
+        roomNodeType = roomNodeTypeList.list[selection];
+
+        if (EditorGUI.EndChangeCheck())
+            EditorUtility.SetDirty(this);
+
+        GUILayout.EndArea();
+    }
+
+    public string[] GetRoomNodeTypesToDisplay()
+    {
+        string[] roomArray = new string[roomNodeTypeList.list.Count];
+
+        for (int i = 0; i < roomNodeTypeList.list.Count; i++)
+        {
+            if (roomNodeTypeList.list[i].displayInNodeGraphEditor)
+            {
+                roomArray[i] = roomNodeTypeList.list[i].roomNodeTypeName;
+            }
+        }
+
+        return roomArray;
+    }
+
+#endif
+
+    #endregion Editor Code
 }
